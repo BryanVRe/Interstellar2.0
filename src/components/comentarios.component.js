058bf834d26f0a0ea68398625f8b3ec54bf8bacb
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-06-01 08:37:19 -0600

    Update comentarios.component.js

--- src/components/comentarios.component.js
+++ src/components/comentarios.component.js
@@ -1,66 +1,49 @@
-import React, {Component} from "react";
-import '../styles/clip.css'
-import KafkaService from "../services/kafka.service";
+import React, { useState } from "react";
 
-class CommentBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: []
-    };
-  }
-  
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const comment = this.refs.comment.value.trim();
-    if (!comment) {
-      return;
-    }
-    this.setState((prevState) => {
-      return {
-        comments: prevState.comments.concat(comment)
-      };
-    });
-    this.refs.commentForm.reset();
-    // Llamar a la función saveComment para enviar el comentario
-    this.saveComment(1,comment);
-  }
-  
-  saveComment = (status, comment) => {
-    let data = {
-      id: 0,
-      status: status
-    };
- 
-    console.log(JSON.stringify(data));
- 
-    KafkaService.comment(this.props.email, this.props.id, comment);
-  }
+const CommentBox = () => {
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
+    const [showForm, setShowForm] = useState(false);
 
-  render() {
-    const { comments } = this.state;
+    const handleNewComment = (event) => {
+        setNewComment(event.target.value);
+    };
+
+    const handleAddComment = () => {
+        if (newComment !== "") {
+            const newCommentObj = {
+                text: newComment
+            };
+            setComments([...comments, newCommentObj]);
+            setNewComment("");
+            setShowForm(false);
+        }
+    };
 
     return (
-      <div>
-        <form ref="commentForm" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="comment">Agregar comentario:</label>
-            <textarea className="form-control" id="comment" rows="3" ref="comment"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">Enviar</button>
-        </form>
-        {comments.length > 0 ?
-          <ul className="list-group list-group-flush">
-            {comments.map((comment, index) => (
-              <li key={index} className="list-group-item">{comment}</li>
-            ))}
-          </ul>
-          :
-          <p>Aun no hay comentarios</p>
-        }
-      </div>
+        <div className="comment-box">
+            <div className="comments">
+                {comments.map((comment, index) => (
+                    <div className="comment" key={index}>
+                        <p className="comment-text">{comment.text}</p>
+                    </div>
+                ))}
+            </div>
+            {showForm ? (
+                <div className="comment-form">
+                    <textarea
+                        value={newComment}
+                        onChange={handleNewComment}
+                        placeholder="Add a comment..."
+                    />
+                    <button onClick={handleAddComment}>Post</button>
+                    <button onClick={() => setShowForm(false)}>Cancel</button>
+                </div>
+            ) : (
+                <button class="btn btn-primary" onClick={() => setShowForm(true)}>Add a comment</button>
+            )}
+        </div>
     );
-  }
-}
+};
 
 export default CommentBox;
